a8bbcaabd9b607a9d33fdbd8506525c9b828a73d
hari.mj <hari.mj@incedoinc.com>
2020-04-24 16:02:05 +0530

    add table

--- src/app/suppliers/suppliers.component.ts
+++ src/app/suppliers/suppliers.component.ts
@@ -17,12 +17,15 @@ export class SuppliersComponent implements OnInit {
   ]
 
   gridViewData = [
-    {name: 'Globex Corporation', email: 'gbcorp@comapp.com', contact: '18156850566', logo: 'w-t1', location: '1919 Davis St, San Leandro, CA 94577', isSelected: true },
-    {name: 'Target', email: 'sherry@socialheit.com', contact: '4567898765', logo: 'target', location: '', isSelected: false },
-    {name: 'Amazon', email: 'jk@amazon.com', contact: '185-656-5656', logo: 'amazon', location: 'New York, NY, USA', isSelected: false },
-    {name: 'Costco', email: 'custservice@costco.com', contact: '5313337559', logo: 'costco', location: 'Wyvern Way, Derby DE21, UK', isSelected: false },
-    {name: 'eBay', email: 'dwenig@ebay.com', contact: '4083767400', logo: 'ebay', location: 'Whittaker House, 2 Whittaker Ave, Richmond TW9 1EH, UK', isSelected: false },
-    {name: 'Lowe\'s', email: 'mark.ellison@lowes.com', contact: '7047581000', logo: 'lowes', location: 'Lowe\'s Blvd Rd, Gaston, NC 27831, USA', isSelected: false },
+    { name: 'Globex Corporation', email: 'gbcorp@comapp.com', contact: '18156850566', logo: 'w-t1', location: '1919 Davis St, San Leandro, CA 94577', isSelected: false },
+    { name: 'ICP', email: 'custservice@icp.com', contact: '4567898765', logo: 'icp', location: '', isSelected: false },
+    { name: 'Reamax', email: 'custservice@reamax.com', contact: '185-656-5656', logo: 'reamax', location: 'New York, NY, USA', isSelected: false },
+    { name: 'Greens Food', email: 'custservice@greens.com', contact: '5313337559', logo: 'greensfood', location: 'Wyvern Way, Derby DE21, UK', isSelected: false },
+    { name: 'Setra', email: 'custservice@setra.com', contact: '5313337559', logo: 'setra', location: 'Wyvern Way, Derby DE21, UK', isSelected: false },
+    { name: 'Veg & More', email: 'custservice@veg&more.com', contact: '4083767400', logo: 'vegandmore', location: 'Whittaker House, 2 Whittaker Ave, Richmond TW9 1EH, UK', isSelected: false },
+    { name: 'Gladiator', email: 'custservice@gladiator.com', contact: '7047581000', logo: 'gladiator', location: 'Lowe\'s Blvd Rd, Gaston, NC 27831, USA', isSelected: false },
+    { name: 'Connxus', email: 'custservice@connxus.com', contact: '4083767400', logo: 'connxus', location: 'Whittaker House, 2 Whittaker Ave, Richmond TW9 1EH, UK', isSelected: false },
+    { name: 'Change Media Group', email: 'custservice@mediagrp.com', contact: '7047581000', logo: 'changemedia', location: 'Lowe\'s Blvd Rd, Gaston, NC 27831, USA', isSelected: false },
   ]
 
   constructor(private titleService: TitleService) { }
